a065f9e6f1d0de010f9ed25a84ca97a8c8402489
Cinderella233 <37975950+Cinderella233@users.noreply.github.com>
2022-04-18 20:54:47 +0800

    Update mobilenet.py

--- classification-pytorch-main/nets/mobilenet.py
+++ classification-pytorch-main/nets/mobilenet.py
@@ -1,3 +1,154 @@
+# from torch import nn
+# from torch.hub import load_state_dict_from_url
+
+
+# __all__ = ['MobileNetV2', 'mobilenet_v2']
+
+
+# model_urls = {
+#     'mobilenet_v2': 'https://download.pytorch.org/models/mobilenet_v2-b0353104.pth',
+# }
+
+
+# def _make_divisible(v, divisor, min_value=None):
+#     if min_value is None:
+#         min_value = divisor
+#     new_v = max(min_value, int(v + divisor / 2) // divisor * divisor)
+#     if new_v < 0.9 * v:
+#         new_v += divisor
+#     return new_v
+
+# class ConvBNReLU(nn.Sequential):
+#     def __init__(self, in_planes, out_planes, kernel_size=3, stride=1, groups=1):
+#         padding = (kernel_size - 1) // 2
+#         super(ConvBNReLU, self).__init__(
+#             nn.Conv2d(in_planes, out_planes, kernel_size, stride, padding, groups=groups, bias=False),
+#             nn.BatchNorm2d(out_planes),
+#             nn.ReLU6(inplace=True)
+#         )
+
+# class InvertedResidual(nn.Module):
+#     def __init__(self, inp, oup, stride, expand_ratio):
+#         super(InvertedResidual, self).__init__()
+#         self.stride = stride
+#         assert stride in [1, 2]
+
+#         hidden_dim = int(round(inp * expand_ratio))
+#         self.use_res_connect = self.stride == 1 and inp == oup
+
+#         layers = []
+#         if expand_ratio != 1:
+#             layers.append(ConvBNReLU(inp, hidden_dim, kernel_size=1))
+#         layers.extend([
+#             ConvBNReLU(hidden_dim, hidden_dim, stride=stride, groups=hidden_dim),
+#             nn.Conv2d(hidden_dim, oup, 1, 1, 0, bias=False),
+#             nn.BatchNorm2d(oup),
+#         ])
+#         self.conv = nn.Sequential(*layers)
+
+#     def forward(self, x):
+#         if self.use_res_connect:
+#             return x + self.conv(x)
+#         else:
+#             return self.conv(x)
+
+
+# class MobileNetV2(nn.Module):
+#     def __init__(self, num_classes=1000, width_mult=1.0, inverted_residual_setting=None, round_nearest=8):
+#         super(MobileNetV2, self).__init__()
+#         block = InvertedResidual
+#         input_channel = 32
+#         last_channel = 1280
+
+#         if inverted_residual_setting is None:
+#             inverted_residual_setting = [
+#                 # t, c, n, s
+#                 # 112, 112, 32 -> 112, 112, 16
+#                 [1, 16, 1, 1],
+#                 # 112, 112, 16 -> 56, 56, 24
+#                 [6, 24, 2, 2],
+#                 # 56, 56, 24 -> 28, 28, 32
+#                 [6, 32, 3, 2],
+#                 # 28, 28, 32 -> 14, 14, 64
+#                 [6, 64, 4, 2],
+#                 # 14, 14, 64 -> 14, 14, 96
+#                 [6, 96, 3, 1],
+#                 # 14, 14, 96 -> 7, 7, 160
+#                 [6, 160, 3, 2],
+#                 # 7, 7, 160 -> 7, 7, 320
+#                 [6, 320, 1, 1],
+#             ]
+
+#         if len(inverted_residual_setting) == 0 or len(inverted_residual_setting[0]) != 4:
+#             raise ValueError("inverted_residual_setting should be non-empty "
+#                              "or a 4-element list, got {}".format(inverted_residual_setting))
+
+#         input_channel = _make_divisible(input_channel * width_mult, round_nearest)
+#         self.last_channel = _make_divisible(last_channel * max(1.0, width_mult), round_nearest)
+
+#         # 224, 224, 3 -> 112, 112, 32
+#         features = [ConvBNReLU(3, input_channel, stride=2)]
+
+#         for t, c, n, s in inverted_residual_setting:
+#             output_channel = _make_divisible(c * width_mult, round_nearest)
+#             for i in range(n):
+#                 stride = s if i == 0 else 1
+#                 features.append(block(input_channel, output_channel, stride, expand_ratio=t))
+#                 input_channel = output_channel
+
+#         # 7, 7, 320 -> 7,7,1280
+#         features.append(ConvBNReLU(input_channel, self.last_channel, kernel_size=1))
+#         self.features = nn.Sequential(*features)
+
+#         self.classifier = nn.Sequential(
+#             nn.Dropout(0.2),
+#             nn.Linear(self.last_channel, num_classes),
+#         )
+
+#         for m in self.modules():
+#             if isinstance(m, nn.Conv2d):
+#                 nn.init.kaiming_normal_(m.weight, mode='fan_out')
+#                 if m.bias is not None:
+#                     nn.init.zeros_(m.bias)
+#             elif isinstance(m, nn.BatchNorm2d):
+#                 nn.init.ones_(m.weight)
+#                 nn.init.zeros_(m.bias)
+#             elif isinstance(m, nn.Linear):
+#                 nn.init.normal_(m.weight, 0, 0.01)
+#                 nn.init.zeros_(m.bias)
+
+#     def forward(self, x):
+#         x = self.features(x)
+#         # 1280
+#         x = x.mean([2, 3])
+#         x = self.classifier(x)
+#         return x
+    
+#     def freeze_backbone(self):
+#         for param in self.features.parameters():
+#             param.requires_grad = False
+
+#     def Unfreeze_backbone(self):
+#         for param in self.features.parameters():
+#             param.requires_grad = True
+
+
+# def mobilenet_v2(pretrained=False, progress=True, num_classes=1000):
+#     model = MobileNetV2()
+#     if pretrained:
+#         state_dict = load_state_dict_from_url(model_urls['mobilenet_v2'], model_dir='./model_data',
+#                                               progress=progress)
+#         model.load_state_dict(state_dict)
+
+#     if num_classes!=1000:
+#         model.classifier = nn.Sequential(
+#                 nn.Dropout(0.2),
+#                 nn.Linear(model.last_channel, num_classes),
+#             )
+#     return model
+
+
+
 from torch import nn
 import torch
 from torch.hub import load_state_dict_from_url
@@ -66,16 +217,16 @@ class SkipBlock(nn.Module):
 
         self.core_block = nn.Sequential(
             # pw
-            nn.Conv2d(inp, hidden_dim, 1, 1, 0, bias=False),
+            Conv2dFix(inp, hidden_dim, 1, 1, 0, bias=False),
             nn.BatchNorm2d(hidden_dim),
             nn.ReLU(inplace=True),
             # dw
-            nn.Conv2d(hidden_dim, hidden_dim, kernel_size, stride, (kernel_size - 1) // 2, groups=hidden_dim, bias=False),
+            Conv2dFix(hidden_dim, hidden_dim, kernel_size, stride, (kernel_size - 1) // 2, groups=hidden_dim, bias=False),
             nn.BatchNorm2d(hidden_dim),
             nn.ReLU(inplace=True),
 
             # pw-linear
-            nn.Conv2d(hidden_dim, out, 1, 1, 0, bias=False),
+            Conv2dFix(hidden_dim, out, 1, 1, 0, bias=False),
             nn.BatchNorm2d(out),
         )
 
@@ -231,7 +382,9 @@ class Conv2dFix(torch.nn.Conv2d):
         self.register_buffer('max_value_in', torch.tensor(0).float())
         self.register_buffer('max_value_out', torch.tensor(0).float())
         self.alpha = 0.95
-        self.record = False
+        self.record = True
+        self.flag = 0
+        self.flag_out = 0
 
     def _conv_forward(self, conv_input, weight, bias):
         if self.padding_mode != 'zeros':
@@ -245,16 +398,18 @@ class Conv2dFix(torch.nn.Conv2d):
         if self.record and self.training:
             # EMA
             with torch.no_grad():
-                if self.max_value_in == 0:
+                if self.flag == 0:
                     self.register_buffer('max_value_in', torch.max(torch.abs(input_f)))
+                    self.flag = 1
                 else:
                     self.register_buffer('max_value_in',
                                          self.max_value_in * self.alpha + torch.max(torch.abs(input_f)) * (
                                                      1 - self.alpha))
             input_f = self.quant_fn(input_f, self.activation_width, self.max_value_in)
         else:
-            if self.max_value_in == 0:
+            if self.flag == 0:
                 input_f = self.quant_fn(input_f, self.activation_width, torch.max(torch.abs(input_f)))
+                self.flag = 1
             else:
                 input_f = self.quant_fn(input_f, self.activation_width, self.max_value_in)
 
@@ -271,15 +426,17 @@ class Conv2dFix(torch.nn.Conv2d):
         if self.record and self.training:
             # EMA
             with torch.no_grad():
-                if self.max_value_out == 0:
+                if self.flag_out == 0:
                     self.register_buffer('max_value_out', torch.max(torch.abs(out)))
+                    self.flag_out = 1
                 else:
                     self.register_buffer('max_value_out',
                                          self.max_value_out * self.alpha + torch.max(torch.abs(out)) * (1 - self.alpha))
             out = self.quant_fn(out, self.activation_width, self.max_value_out)
         else:
-            if self.max_value_out == 0:
+            if self.flag_out == 0:
                 out = self.quant_fn(out, self.activation_width, torch.max(torch.abs(out)))
+                self.flag_out = 1
             else:
                 out = self.quant_fn(out, self.activation_width, self.max_value_out)
         return out
